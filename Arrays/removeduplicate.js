79b98b85a697f7e70d2179a15e517336baffee67
reenadangi <reena.dangi@gmail.com>
2020-03-07 11:33:41 -0600

    updated remove duplicate

--- Arrays/removeduplicate.js
+++ Arrays/removeduplicate.js
@@ -2,31 +2,22 @@ function removeDuplicates(arr)
 {
   var temp=[];
   var x=0;
-  var duplicate=0;
-  for (var i=0; i<arr.length; i++) 
-    {
-      for(var j=i;j<arr.length;j++)
-        {
-          if(arr[i]===arr[j])
-            {
-              duplicate=1;
-            }
-        }
-        if(duplicate!==1)
-             {
-               console.log(arr[i], "Not Duplicate");
-               temp[x]=arr[i];
-               x++;
-               duplicate=0; 
-              }
-     
-      else
-        {console.log(arr[i], " Duplicate");
-          duplicate=0;
-        }
-        
+  var duplicate=false;
+  for(var i=0;i<arr.length;i++){
+      for(var j=0;j<temp.length;j++){
+          if(arr[i]==temp[j])
+          {
+               duplicate=true; 
+               break;
+          }
+      }
+      if(!duplicate){
+        temp[x]=arr[i];
+        duplicate=false;
+        x++;
     }
-  
+    duplicate=false;
+  }
   console.log(temp);
 }
 removeDuplicates([2,3,4,2,4,5,6]);
