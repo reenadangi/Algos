b8b94578d90dfedf58797ad866cb18c04918bdbb
reenadangi <reena.dangi@gmail.com>
2020-03-12 23:10:50 -0500

    BOT commit

---
+++ Arrays/rotateFromStep.js
@@ -0,0 +1,15 @@
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+// Input: [1,2,3,4,5,6,7] and k = 3
+
+       
+// Output: [5,6,7,1,2,3,4]
+function rotate(nums,k){
+    var endPoint=nums.length-1;
+    for(var i=0;i<k;i++)
+    {
+        
+
+    }
+    console.log(nums)
+}
+rotate([1,2,3,4,5,6,7],3);
