483ad4ce6942b7b5f9f2fa21e4c69f1dac75b03c
reenadangi <reena.dangi@gmail.com>
2020-03-22 16:57:58 -0500

    reverse string II

--- String/Solution.java
+++ String/Solution.java
@@ -3,8 +3,11 @@ class Solution {
     public static void main(String[] args) {
         char[] c= {'h','e','l','l','o'};
         reverseString(c);
+        // display(c);
+        display(reverseStringTwo("abcdefg", 2).toCharArray());
     }
     public static void reverseString(char[] s) {
+     
         int start=0;
         int end=s.length-1;
        while(start<end){
@@ -14,9 +17,27 @@ class Solution {
             start++;
             end--;
         }
+    }
+    public static void display(char[] s){
         for (char c : s) {
             System.out.println(c);
         }
-
     }
+    // Input: s = "abcdefg", k = 2
+    // Output: "bacdfeg"
+    public static String reverseStringTwo(String s,int k){
+        char[] a=s.toCharArray();
+        for(int start=0;start<a.length;start+=2*k){
+            int i=start;
+            int j=Math.min(start + k - 1, a.length - 1);
+            while(i<j){
+                char temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+            i++;
+            j--;
+            }
+        }
+        return s=new String(a);
     }
+}
